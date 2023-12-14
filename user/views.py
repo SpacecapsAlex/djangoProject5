1bbaccc8792942c88fd0113c412b56fe447710b1
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-12-14 21:21:04 +0300

    Add User model and update views

--- user/views.py
+++ user/views.py
@@ -1,5 +1,9 @@
+from django.forms import forms
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+
+from user.models import User
+
 
 # Create your views here.
 
@@ -11,4 +15,8 @@ def home(request: HttpRequest) -> HttpResponse:
 def method(request: HttpRequest) -> HttpResponse:
     name = request.POST.get('name', 'undefined')
     email = request.POST.get('email', 'undefined')
+
+    user = User.objects.create(name=name, email=email)
+    user.save()
+
     return HttpResponse(f'Name: {name}; Email: {email}')
